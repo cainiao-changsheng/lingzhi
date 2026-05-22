72872ac2c6f915f4063537894f61805bc11b07e6
liyina_ai <2257467528@qq.com>
2026-05-22 20:07:24 +0800

    fix: 添加缺失的 foundation 导入以修复 debugPrint 错误

--- lib/services/image_generation_service.dart
+++ lib/services/image_generation_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
